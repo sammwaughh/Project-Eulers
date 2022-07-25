21714f8c8139fdd198d6d69f762f12ccaaf4b917
Sam Waugh <samjmwaugh@gmail.com>
2022-07-25 23:35:07 +0100

    completed

--- problem-8.py
+++ problem-8.py
@@ -2,6 +2,20 @@
 
 print("Running...")
 
+numFile = open("8.txt", "r")
+num = numFile.read()
+num = num.replace("\n", "")
 
+maxp = 1
+
+for i in range(987):
+    s = num[i:i+13]
+    p = 1
+    for c in s:
+        p *= int(c)
+    if p > maxp:
+        maxp = p
+
+print(maxp)
 
 print("...End")
