7dce1b3be70f10fae95fa7c4816f08d0e69b109c
Sam Waugh <samjmwaugh@gmail.com>
2022-07-26 12:45:59 +0100

    completed

--- problem-12.py
+++ problem-12.py
@@ -1,7 +1,28 @@
-# Problem 11
+# Problem 12
+import math
 
 print("Running...")
 
+def numFactors(n):
+    sqrtn = math.sqrt(n)
+    upperBound = math.floor(sqrtn)
+    count = 2
+    for i in range(2, upperBound+1):
+        if n % i == 0:
+            count += 2
+    if sqrtn == upperBound:
+        count -= 1
+    return count
 
+found = False
+t = 0
+i = 1
+while not found:
+    t += i
+    i += 1
+    if numFactors(t) > 500:
+        found = True
+
+print(t)
 
 print("...End")
