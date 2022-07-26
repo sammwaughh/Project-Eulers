72907f22fd61e5c44047e4ae3d29ac8dd50a3d0e
Sam Waugh <samjmwaugh@gmail.com>
2022-07-26 14:10:06 +0100

    completed

---
+++ problem-15.py
@@ -0,0 +1,13 @@
+# Problem 15
+# More mathematical
+# Use choosing logic
+# 20 ups, 20 downs, in some unqiue order
+# Therefore answer is 40C20
+
+from math import comb
+
+print("Running...")
+
+print(comb(40, 20))
+
+print("...End")
