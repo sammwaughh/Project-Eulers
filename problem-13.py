4b13d3bb720463b9ed609810f9257471d1d71e45
Sam Waugh <samjmwaugh@gmail.com>
2022-07-26 12:51:37 +0100

    completed

--- problem-13.py
+++ problem-13.py
@@ -1,8 +1,14 @@
-# Problem 12
-import math
+# Problem 13
 
 print("Running...")
 
+file13 = open("13.txt", "r")
+strNums = file13.readlines()
+sum = 0
+for s in strNums:
+    sum += int(s.strip())
 
+print(sum)
+# Read the first 10 digits
 
 print("...End")
