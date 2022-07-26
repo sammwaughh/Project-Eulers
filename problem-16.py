e891750f3be3b38df06057e4e623188080a14a19
Sam Waugh <samjmwaugh@gmail.com>
2022-07-26 14:14:38 +0100

    completed

--- problem-16.py
+++ problem-16.py
@@ -2,6 +2,15 @@
 
 print("Running...")
 
+twoToThousand = 2**1000
 
+def sumDigits(n):
+    s = str(n)
+    sum = 0
+    for c in s:
+        sum += int(c)
+    return sum
+
+print(sumDigits(twoToThousand))
 
 print("...End")
