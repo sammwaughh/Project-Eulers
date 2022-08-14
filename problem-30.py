35c1a99cc274234a4336056275dda3bab5fc56d2
Sam Waugh <samjmwaugh@gmail.com>
2022-08-14 20:04:20 +0100

    completed

--- problem-30.py
+++ problem-30.py
@@ -1,9 +1,25 @@
-# Problem 27
-
-import math
+# Problem 30
 
 print("Running...")
 
+# Guessed that there are none over 1 million
+# Sort of hackish
 
+def isFivePowerSum(n):
+    s = str(n)
+    total = 0
+    for c in s:
+        total += int(c)**5
+    if total == n:
+        return True
+    else:
+        return False
+
+sum = 0
+for i in range(2,1000000):
+    if isFivePowerSum(i):
+        sum += i
+
+print(sum)
 
 print("...End")
