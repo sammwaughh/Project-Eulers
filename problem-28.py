cfa8b6de816e5b8b3e8ba6a1d4a00346c9908182
Sam Waugh <samjmwaugh@gmail.com>
2022-08-14 19:44:09 +0100

    completed

---
+++ problem-28.py
@@ -0,0 +1,14 @@
+# Problem 27
+
+print("Running...")
+
+powers = []
+for i in range(2, 101):
+    for j in range(2, 101):
+        k = i**j
+        if k not in powers:
+            powers.append(k)
+
+print(len(powers))
+
+print("...End")
