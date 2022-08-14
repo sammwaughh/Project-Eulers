8d977e92ce71a32208b295a6eca9f6520eb1e798
Sam Waugh <samjmwaugh@gmail.com>
2022-08-14 16:56:54 +0100

    completed

--- problem-26.py
+++ problem-26.py
@@ -1,7 +1,38 @@
 # Problem 26
 
+from math import remainder
+from socket import NI_NUMERICHOST
+
+
 print("Running...")
 
+def lengthOfRepeat(n):
+    count = 1
+    remainders = [1]
+    stop = False
+    while not stop:
+        numerator = remainders[-1]
+        while numerator < n:
+            numerator *= 10
+        remainder = numerator % n
+        if remainder == 0:
+            stop = True
+        if remainder in remainders:
+            stop = True
+        else:
+            remainders.append(remainder)
+            count += 1
+    return count
 
+bestD = 1
+longestCycle = 1
+
+for i in range(1, 1000):
+    cycleLen = lengthOfRepeat(i)
+    if cycleLen > longestCycle:
+        bestD = i
+        longestCycle = cycleLen
+
+print(bestD)
 
 print("...End")
