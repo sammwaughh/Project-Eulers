04b59876362cf9c19963aac545c701eaf92273df
Sam Waugh <samjmwaugh@gmail.com>
2022-08-14 19:46:51 +0100

    starting 27

--- problem-27.py
+++ problem-27.py
@@ -2,18 +2,6 @@
 
 print("Running...")
 
-difference = 2
-top = 1001*1001
-i = 1
-diagonals = []
-count = 0
-while i <= top:
-    diagonals.append(i)
-    i += difference
-    count += 1
-    if count % 4 == 0:
-        difference += 2
 
-print(sum(diagonals))
 
 print("...End")
