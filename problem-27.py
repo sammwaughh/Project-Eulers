3530d6cbc58d792af58185ff0cf9e3dd0be38fb5
Sam Waugh <samjmwaugh@gmail.com>
2022-08-14 19:51:09 +0100

    started

--- problem-27.py
+++ problem-27.py
@@ -1,6 +1,19 @@
 # Problem 27
 
+import math
+
 print("Running...")
+
+def isPrime(n):
+    sqrtN = math.floor(math.sqrt(n))
+    if n == 2:
+        return True
+    if n % 2 == 0 or n==1 or n==0:
+        return False
+    for i in range(3, sqrtN+1, 2):
+        if n % i == 0:
+            return False
+    return True
 
 
 
