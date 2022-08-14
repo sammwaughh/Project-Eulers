73178a4186d334223b5f911df178a521714ea484
Sam Waugh <samjmwaugh@gmail.com>
2022-08-14 19:46:32 +0100

    done 28

--- problem-28.py
+++ problem-28.py
@@ -2,13 +2,18 @@
 
 print("Running...")
 
-powers = []
-for i in range(2, 101):
-    for j in range(2, 101):
-        k = i**j
-        if k not in powers:
-            powers.append(k)
+difference = 2
+top = 1001*1001
+i = 1
+diagonals = []
+count = 0
+while i <= top:
+    diagonals.append(i)
+    i += difference
+    count += 1
+    if count % 4 == 0:
+        difference += 2
 
-print(len(powers))
+print(sum(diagonals))
 
 print("...End")
