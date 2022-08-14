d35dc1dce22646111efd7da44f77d759ae2dba69
Sam Waugh <samjmwaugh@gmail.com>
2022-08-14 12:20:03 +0100

    done 25

--- problem-25.py
+++ problem-25.py
@@ -2,6 +2,22 @@
 
 print("Running...")
 
+def numDigits(n):
+    s = str(n)
+    return len(s)
 
+a = 1
+b = 1
+i = 2
+stop = False
+while not stop:
+    if numDigits(b) == 1000:
+        print(i, b)
+        stop = True
+    else:
+        t = a + b
+        a = b
+        b = t
+        i += 1
 
 print("...End")
