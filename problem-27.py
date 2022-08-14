bedfd7f2b346f3796d2960fec4ac0c62bf8e5e77
Sam Waugh <samjmwaugh@gmail.com>
2022-08-14 19:58:13 +0100

    completed

--- problem-27.py
+++ problem-27.py
@@ -5,16 +5,37 @@ import math
 print("Running...")
 
 def isPrime(n):
+    if n <= 1 or n%2 == 0:
+        return False
     sqrtN = math.floor(math.sqrt(n))
     if n == 2:
         return True
-    if n % 2 == 0 or n==1 or n==0:
-        return False
     for i in range(3, sqrtN+1, 2):
         if n % i == 0:
             return False
     return True
 
+def computeQuadratic(n, a, b):
+    val = n**2 + n*a + b
+    return val
 
+bestCount = 0
+bestPair = None
+
+for a in range(-1000, 1001):
+    for b in range(-1000, 1001):
+        n = 0
+        stop = False
+        while not stop:
+            if isPrime(computeQuadratic(n, a, b)):
+                n += 1
+            else:
+                stop = True
+        if n > bestCount:
+            bestCount = n
+            bestPair = (a, b, n)
+
+print(bestPair)
+print(bestPair[0]*bestPair[1])
 
 print("...End")
