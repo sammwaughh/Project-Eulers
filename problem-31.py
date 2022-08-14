55c73015e067c92c47c3d57bb0bcf8f10e35c813
Sam Waugh <samjmwaugh@gmail.com>
2022-08-14 20:40:27 +0100

    progress made

--- problem-31.py
+++ problem-31.py
@@ -2,6 +2,23 @@
 
 print("Running...")
 
+def compareSets(A, B):
+    pass
 
+def distinctSet(S):
+    pass
+
+def addingCombos(target, set):
+    print(target, set)
+    combos = []
+    for ele in set:
+        if ele == target:
+            combos.append([ele])
+        elif ele < target:
+            for ele2 in addingCombos(target - ele, set):
+                combos.append([ele] + ele2)
+    return combos
+
+print(addingCombos(4, [1,2]))
 
 print("...End")
