e98e3ccad52c48c0521daee8025fcd061ff17aaf
Sam Waugh <samjmwaugh@gmail.com>
2022-08-15 13:43:58 +0100

    completed

--- problem-34.py
+++ problem-34.py
@@ -2,8 +2,23 @@
 
 import math
 
-print("Running.. .")
+print("Running...")
 
+# Guessed there were no more after 10,000,000
+# Assumption was correct. Answer is 145 + 40585 == 40730
 
+def digitFactorial(n):
+    s = str(n)
+    sum = 0
+    for c in s:
+        sum += math.factorial(int(c))
+    return sum
+
+sum = 0
+for i in range(10, 10000000):
+    if i == digitFactorial(i):
+        sum += i
+
+print(sum)
 
 print("...End")
