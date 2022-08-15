be36d2c980b189a01ae393f2ff66b6936082359e
Sam Waugh <samjmwaugh@gmail.com>
2022-08-15 13:24:09 +0100

    completed

--- problem-32.py
+++ problem-32.py
@@ -4,28 +4,45 @@ print("Running...")
 
 # Generate a list of lists. Each element is a list of the digits 1-9
 # which represents a unique permuation of those digits
-permutations = []
-
-digits = [1,2,3,4]
-bins = []
-for i in range(2**9):
-    bins.append(str(bin(i))[2:].zfill(9))
-
-startSet = [1,2,3,4]
 
 def producePerms(set):
-    if set == None or len(set) == 0:
-        return []
+    if len(set) == 1:
+        return [set]
     else:
         perms = []
-        for ele in set:
-            setdash = set.remove(ele)
+        for i in range(len(set)):
+            setdash = set.copy()
+            ele = set[i]
             first = [ele]
-            for smallerSet in producePerms(setdash):
+            setdash.remove(ele)
+            smallerPerms = producePerms(setdash)
+            for smallerSet in smallerPerms:
                 perms.append(first + smallerSet)
         return perms
-        
-print(producePerms(startSet))
-            
+
+nineDigitSet = [1,2,3,4,5,6,7,8,9]       
+nineDigitPerms = producePerms(nineDigitSet)
+
+def arrToInt(arr):
+    num = 0
+    l = len(arr)
+    for i in range(l):
+        num += arr[i] * 10**(l-i-1)
+    return num
+
+prods = []
+
+for perm in nineDigitPerms:
+    p1 = arrToInt(perm[5:9])
+    m1 = arrToInt(perm[:4])
+    m2 = perm[4]
+    m3 = arrToInt(perm[:2])
+    m4 = arrToInt(perm[2:5])
+    if m1*m2 == p1 or m3*m4 == p1:
+        if p1 not in prods:
+            prods.append(p1)
+
+print(prods)
+print(sum(prods))
 
 print("...End")
