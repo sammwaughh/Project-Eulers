dfc44ccff0fe5a9427e3b078992b4b9c997da03a
Sam Waugh <samjmwaugh@gmail.com>
2022-08-16 11:01:18 +0100

    progress

--- problem-41.py
+++ problem-41.py
@@ -1,7 +1,24 @@
-# Problem 40
+# Problem 41
+
+import math
 
 print("Running...")
 
+def isPrime(n):
+    sqrtN = math.floor(math.sqrt(n))
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, sqrtN+1, 2):
+        if n % i == 0:
+            return False
+    return True
 
+primes = [2,3]
+for i in range(4, 1000000):
+    if isPrime(i):
+        primes.append(i)
+print(primes)
 
 print("...End")
