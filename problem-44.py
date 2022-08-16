0185e4f38ff5b4a920a86451682fae2f75577a55
Sam Waugh <samjmwaugh@gmail.com>
2022-08-16 12:51:15 +0100

    completed

--- problem-44.py
+++ problem-44.py
@@ -1,7 +1,30 @@
 # Problem 44
 
+from cgitb import small
+
+
 print("Running...")
 
+def genPentagonals(n):
+    pents = []
+    for i in range(1, n+1):
+        pents.append((3*i*i - i)//2)
+    return pents
 
+pents = genPentagonals(3000)
+
+smallestD = 0
+for i in range(len(pents)):
+    for j in range(i+1, len(pents)):
+        d = pents[j] - pents[i]
+        if d in pents:
+            s = pents[j] + pents[i]
+            if s in pents:
+                if d < smallestD or smallestD == 0:
+                    smallestD = d
+print(smallestD)
+# Works. Trial and Error finds answer works 
+# for below 3000th pentagonal number
+# Takes a minute
 
 print("...End")
