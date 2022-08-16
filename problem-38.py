1c7ed1eb35459559efc6765f17ea4f3c43149a87
Sam Waugh <samjmwaugh@gmail.com>
2022-08-16 10:31:16 +0100

    progress

--- problem-38.py
+++ problem-38.py
@@ -2,6 +2,25 @@
 
 print("Running...")
 
+def makeNum(i, n):
+    s = ""
+    for j in range(1, n+1):
+        s += str(i*j)
+    return int(s)
+
+def isPandigital(n):
+    s = str(n)
+    if len(s) != 9:
+        return False
+    digs = [1,2,3,4,5,6,7,8,9]
+    for c in s:
+        intc = int(c)
+        if intc not in digs:
+            return False
+        else:
+            digs.remove(intc)
+    return True
+
 
 
 print("...End")
