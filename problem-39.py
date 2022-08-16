8c1ecbef57dba3b157a6e1ed68107d79e7eb93b3
Sam Waugh <samjmwaugh@gmail.com>
2022-08-16 10:41:20 +0100

    started

---
+++ problem-39.py
@@ -0,0 +1,13 @@
+# Problem 39
+
+print("Running...")
+
+def isRightTriangle(a, b, c):
+    if a**2 + b**2 == c**2:
+        return True
+    else:
+        return False
+
+print(isRightTriangle(3,4,5))
+
+print("...End")
