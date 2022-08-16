288084950c534829b8b7baedd12b295a07a59e7e
Sam Waugh <samjmwaugh@gmail.com>
2022-08-16 10:54:10 +0100

    completed

--- problem-39.py
+++ problem-39.py
@@ -2,12 +2,41 @@
 
 print("Running...")
 
-def isRightTriangle(a, b, c):
-    if a**2 + b**2 == c**2:
+def isRightTriangle(tup):
+    if tup[0]**2 + tup[1]**2 == tup[2]**2:
         return True
     else:
         return False
 
-print(isRightTriangle(3,4,5))
+def generateTriangles(p):
+    triangles = []
+    m = p//2
+    # a in the length of the base
+    for a in range(1, m+1):
+        # rem is the length left for the other two sides
+        rem = p - a
+        # b is the length of the second side
+        for b in range(1, rem):
+            # c is the length of the third side
+            c = rem - b
+            triangles.append((a,b,c))
+    return triangles
+
+def right(triangles):
+    ts = []
+    for t in triangles:
+        if isRightTriangle(t):
+            ts.append(t)
+    return ts
+
+mostTs = 0
+bestI = 0
+for i in range(10, 1001):
+    s = len(right(generateTriangles(i)))
+    if s > mostTs:
+        mostTs = s
+        bestI = i
+# Takes about 2 mins
+print(bestI)
 
 print("...End")
