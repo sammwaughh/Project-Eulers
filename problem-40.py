5fb77636fa57ed7661f2698a662fea22484764ef
Sam Waugh <samjmwaugh@gmail.com>
2022-08-16 10:59:29 +0100

    done

--- problem-40.py
+++ problem-40.py
@@ -2,6 +2,13 @@
 
 print("Running...")
 
+bigs = ""
+i = 1
+while len(bigs) < 1000000:
+    bigs += str(i)
+    i += 1
 
+val = int(bigs[0]) * int(bigs[9]) * int(bigs[99]) * int(bigs[999]) * int(bigs[9999]) * int(bigs[99999]) * int(bigs[999999])
+print(val)
 
 print("...End")
