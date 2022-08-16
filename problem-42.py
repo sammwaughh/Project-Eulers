90af55dee4586d2070b20ff9438d591b6165ed86
Sam Waugh <samjmwaugh@gmail.com>
2022-08-16 11:29:12 +0100

    done

--- problem-42.py
+++ problem-42.py
@@ -2,6 +2,27 @@
 
 print("Running...")
 
+words = open("42.txt", "r")
+wordList = words.read()[1:-1].split("\",\"")
+words.close()
 
+ts = [1]
+i = 2
+while ts[-1] < 300:
+    ts.append(ts[-1]+i)
+    i += 1
+
+def wordValue(word):
+    sum = 0
+    for c in word:
+        sum += ord(c) - 64
+    return sum
+
+count = 0
+for word in wordList:
+    if wordValue(word) in ts:
+        count += 1
+
+print(count)
 
 print("...End")
