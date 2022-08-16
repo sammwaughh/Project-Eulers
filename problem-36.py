b8e2e99f932c5cb6a36b58b6b7348a904b1b4158
Sam Waugh <samjmwaugh@gmail.com>
2022-08-16 09:50:23 +0100

    completed

--- problem-36.py
+++ problem-36.py
@@ -2,6 +2,24 @@
 
 print("Running...")
 
+def isPalindrome(s):
+    r = ""
+    for i in range(len(s)-1, -1, -1):
+        r += s[i]
+    if r == s:
+        return True
+    else:
+        return False
+
+def makeBin(n):
+    return str(bin(n))[2:]
+
+sum = 0
+for i in range(1000000):
+    if isPalindrome(str(i)):
+        if isPalindrome(makeBin(i)):
+            sum += i
+print(sum)
 
 
 print("...End")
