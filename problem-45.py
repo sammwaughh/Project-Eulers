8eb18347bc8a4024263ca5a2f28e837c6891f164
Sam Waugh <samjmwaugh@gmail.com>
2022-08-16 12:59:02 +0100

    done

--- problem-45.py
+++ problem-45.py
@@ -2,6 +2,28 @@
 
 print("Running...")
 
+ts = []
+ps = []
+hs = []
 
+def makeNthT(n):
+    return (n*(n+1))//2
+
+def makeNthP(n):
+    return (n*(3*n - 1))//2
+
+def makeNthH(n):
+    return n*(2*n - 1)
+
+for i in range(1, 100000):
+    ts.append(makeNthT(i))
+    ps.append(makeNthP(i))
+    hs.append(makeNthH(i))
+
+for t in ts:
+    if t in ps and t in hs:
+        print(t)
+
+# Works, takes a min
 
 print("...End")
