b230d31e15287d65f71a09630ce1b40399c965a4
Sam Waugh <samjmwaugh@gmail.com>
2022-08-18 11:01:44 +0100

    progress

--- problem-51.py
+++ problem-51.py
@@ -2,6 +2,29 @@
 
 print("Running...")
 
+### Efficient Prime list generator
+
+def isPrime(n, knownPrimes):
+    sqrtN = math.ceil(math.sqrt(n))
+    i = 0
+    tryp = 2
+    while tryp < sqrtN:
+        tryp = knownPrimes[i]
+        if n % tryp == 0:
+            return False
+        i += 1
+    return True
+
+primes = [2,3]
+
+for i in range(5, 1000000, 2):
+    if isPrime(i, primes):
+        primes.append(i)
+
+### Efficient Prime list generator
+### Don't edit
+# primes is all primes under 1,000,000
+
 
 
 print("...End")
