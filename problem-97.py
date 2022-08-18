5e795dc6cb6d8ed775243e6d188a2fa15233be4e
Sam Waugh <samjmwaugh@gmail.com>
2022-08-18 20:37:00 +0100

    done

--- problem-97.py
+++ problem-97.py
@@ -2,7 +2,17 @@
 
 print("Running...")
 
+# 2^30 = 1073741824
+power2 = 1073741824
 
-
+j = 30
+arr = []
+while j < 7830457:
+    arr.append(power2)
+    newPower = power2*2
+    power2 = int(str(newPower)[-10:])
+    j += 1
+num = power2*28433 + 1
+print(str(num)[-10:])
 
 print("...End")
