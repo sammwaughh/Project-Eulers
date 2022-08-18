ce651c1a042ea63afa9e59e7eed47f5b7655ff5b
Sam Waugh <samjmwaugh@gmail.com>
2022-08-18 10:56:50 +0100

    done

--- problem-50.py
+++ problem-50.py
@@ -1,8 +1,53 @@
 # Problem 50
 
+import math
+
 print("Running...")
 
+### Efficient Prime list generator
 
+def isPrime(n, knownPrimes):
+    sqrtN = math.ceil(math.sqrt(n))
+    i = 0
+    tryp = 2
+    while tryp < sqrtN:
+        tryp = knownPrimes[i]
+        if n % tryp == 0:
+            return False
+        i += 1
+    return True
+
+primes = [2,3]
+
+for i in range(5, 1000000, 2):
+    if isPrime(i, primes):
+        primes.append(i)
+
+### Efficient Prime list generator
+### Don't edit
+# primes is all primes under 1,000,000
+
+bestL = 1
+bestP = 1
+last = primes[-1]
+for i in range(len(primes)):
+    j = 1
+    arr = primes[i:i+j]
+    s = sum(arr)
+    l = len(arr)
+    while s < last:
+        if s in primes:
+            if l > bestL:
+                bestL = l
+                bestP = s
+        j += 1
+        arr = primes[i:i+j]
+        s = sum(arr)
+        l = len(arr)
+
+#Takes a minute
+
+print(bestL, bestP)
 
 print("...End")
 
