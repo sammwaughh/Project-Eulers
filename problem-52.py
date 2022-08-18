db985255f4b8e53794f03aa605137fd8f431b665
Sam Waugh <samjmwaugh@gmail.com>
2022-08-18 14:14:41 +0100

    done

--- problem-52.py
+++ problem-52.py
@@ -2,6 +2,33 @@
 
 print("Running...")
 
+def sameDigits(a, b):
+    sa = str(a)
+    sb = str(b)
+    arr1 = []
+    arr2 = []
+    for c in sa:
+        arr1.append(c)
+    for c in sb:
+        arr2.append(c)
+    for ele in arr1:
+        if ele not in arr2:
+            return False
+        else:
+            arr2.remove(ele)
+    if len(arr2) == 0:
+        return True
+    else:
+        return False
 
+i = 1
+found = False
+while not found:
+    if sameDigits(3*i, 2*i):
+        if sameDigits(4*i, 5*i):
+            if sameDigits(2*i, 6*i):
+                found = True
+                print(i)
+    i += 1
 
 print("...End")
