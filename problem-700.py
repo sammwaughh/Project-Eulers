07c8d055b60c21ce570e1695a50c24b6a97d2fe8
Sam Waugh <samjmwaugh@gmail.com>
2022-08-18 23:02:19 +0100

    progress

--- problem-700.py
+++ problem-700.py
@@ -2,6 +2,21 @@
 
 print("Running...")
 
+def nthEulerCoin(n):
+    return (1504170715041707*n)%4503599627370517
 
+sum = 1517926264989119
+i = 1000000000
+smallestCoin = 14578937
+bound = 2000000000
+while i <= bound:
+    eulerCoin = nthEulerCoin(i)
+    if eulerCoin < smallestCoin:
+        smallestCoin = eulerCoin
+        sum += eulerCoin
+    i += 1
+
+print(sum)
+print(smallestCoin)
 
 print("...End")
