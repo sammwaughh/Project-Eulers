71395116c23a034dd8cc873fc5ec5b7f38279fdf
Sam Waugh <samjmwaugh@gmail.com>
2022-08-18 20:16:45 +0100

    done

--- problem-53.py
+++ problem-53.py
@@ -4,6 +4,21 @@ import math
 
 print("Running...")
 
+factorials = []
+for i in range(101):
+    factorials.append(math.factorial(i))
+
+def nChooseR(n, r):
+    return (factorials[n])//(factorials[r]*factorials[n-r])
+
+count = 0
+for n in range(1, 101):
+    r = 1
+    while r <= n:
+        if nChooseR(n, r) > 1000000:
+            count += 1
+        r += 1
+print(count)
 
 
 print("...End")
