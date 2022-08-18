b84574861d63c33c41016fa8107b6f731fa38f4b
Sam Waugh <samjmwaugh@gmail.com>
2022-08-18 20:41:18 +0100

    done

--- problem-56.py
+++ problem-56.py
@@ -2,6 +2,20 @@
 
 print("Running...")
 
+def digitSum(n):
+    s = str(n)
+    sum = 0
+    for c in s:
+        sum += int(c)
+    return sum
 
+bestSum = 0
+for a in range(1, 101):
+    for b in range(1, 101):
+        p = a**b
+        d = digitSum(p)
+        if d > bestSum:
+            bestSum = d
+print(bestSum)
 
 print("...End")
