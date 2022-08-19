f85f04f3b864e30f1a3ee2819bf2aac3f879ff2e
Sam Waugh <samjmwaugh@gmail.com>
2022-08-19 20:17:28 +0100

    naive attempt

--- problem-684.py
+++ problem-684.py
@@ -20,8 +20,22 @@ def s(n):
             i += 1
     return i
 
+def S(k):
+    sum = 0
+    for n in range(1, k+1):
+        sum += s(n)
+    return sum
 
-
-
+sum = 0
+a = 0
+b = 1
+i = 1
+while i < 90:
+    t = a + b
+    a = b
+    b = t
+    # sum += S(b)
+    i += 1
+print(S(2880067194370816120))
 
 print("...End")
