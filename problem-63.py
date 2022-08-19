657b17aa51993575bf02f4c5005b9128e1cf52cf
Sam Waugh <samjmwaugh@gmail.com>
2022-08-19 12:13:19 +0100

    done

--- problem-63.py
+++ problem-63.py
@@ -2,6 +2,26 @@
 
 print("Running...")
 
+def digitCount(n):
+    return len(str(n))
 
+count = 0
+nums = []
+for n in range(1, 100):
+    base = 1
+    done = False
+    while not done:
+        num = base**n
+        digits = digitCount(num)
+        if digits == n:
+            nums.append(num)
+            count += 1
+        elif digits > n:
+            done = True
+        base += 1
+        
+print(count)
+
+# Works
 
 print("...End")
