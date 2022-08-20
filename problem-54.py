e5d19070f250cd9fea383e10baf084562f3981fd
Sam Waugh <samjmwaugh@gmail.com>
2022-08-20 10:30:26 +0100

    dealt with file

--- problem-54.py
+++ problem-54.py
@@ -2,6 +2,12 @@
 
 print("Running...")
 
-
+file54 = open("54.txt", "r")
+arr = file54.read().split("\n")
+arr2 = []
+for ele in arr:
+    arr2.append(ele.split(" "))
+hands = arr2[:-1]
+print(hands)
 
 print("...End")
