baef01af019ad85830f146f8c353c562e21d072c
Sam Waugh <samjmwaugh@gmail.com>
2022-08-25 15:19:23 +0100

    done

--- problem-1.c
+++ problem-1.c
@@ -1,8 +1,19 @@
 #include <string.h>
 #include <stdio.h>
 
+int findSum(){
+    int sum = 0;
+    for (int i = 0; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
 
 int main(){
-    printf("Hello World\n");
+    int sum = findSum();
+    printf("%d\n", sum);
     return 0;
 }
