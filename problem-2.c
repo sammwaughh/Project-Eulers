b672b42197d147c1ead2fa9c5fc45ee0812db141
Sam Waugh <samjmwaugh@gmail.com>
2022-08-25 15:23:02 +0100

    done

--- problem-2.c
+++ problem-2.c
@@ -3,6 +3,17 @@
 
 
 int main(){
-    
+    int a = 1;
+    int b = 1;
+    int sum = 0;
+    while (b < 4000000) {
+        int t = a + b;
+        a = b;
+        b = t;
+        if (t % 2 == 0) {
+            sum += t;
+        }
+    }
+    printf("%d\n", sum);
     return 0;
 }
