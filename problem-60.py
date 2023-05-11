9137e513d25665b9a21322c43015dc9e2dd4b51d
Samuel Waugh <samjmwaugh@gmail.com>
2023-05-11 22:43:52 +0100

    Done 60

--- problem-60.py
+++ problem-60.py
@@ -20,48 +20,52 @@ def primes_less_than(n):
                 is_prime[x] = False
     return is_prime
 
-prime_bools = primes_less_than(1000)
+prime_bools = primes_less_than(10000)
 prime_nums = [i for i in range(len(prime_bools)) if prime_bools[i]]
-is_prime = primes_less_than(1000000)
+is_prime = primes_less_than(100000000)
 
-l = len(prime_nums)
+print("Calculated primes.")
 
-print("Done calculating primes")
+def evaluate_pair(pair):
+    q1 = pair[0]
+    q2 = pair[1]
+    p1 = int(str(q1) + str(q2))
+    p2 = int(str(q2) + str(q1))
+    if is_prime[p1] and is_prime[p2]:
+        return True
 
-def evaluate_set(set):
-    for i in range(len(set)):
-        for j in range(i+1, len(set)):
-            p1 = int(str(set[i]) + str(set[j]))
-            p2 = int(str(set[j]) + str(set[i]))
-            if not is_prime[p1]:
-                return False
-            if not is_prime[p2]:
-                return False
-    return True
+good_pairs = []
+for i in range(len(prime_nums)):
+    for j in range(i+1, len(prime_nums)):
+        pair = [prime_nums[i], prime_nums[j]]
+        if evaluate_pair(pair):
+            good_pairs.append(pair)
 
-def sort_list_of_tuples(lst):
-    sorted_list = sorted(lst, key=lambda x: x[1])
-    return sorted_list
+good_trips = []
+for pair in good_pairs:
+    start = pair[1]
+    for x in prime_nums:
+        if x > start:
+            if evaluate_pair([pair[0], x]) and evaluate_pair([pair[1], x]):
+                good_trips.append([pair[0], pair[1], x])
 
-prime_sets = []
-for i in range(l):
-    for j in range(i+1, l):
-        print(i, j)
-        for k in range(j+1, l):
-            for m in range(k+1, l):
-                for v in range(m+1, l):
-                    set = [prime_nums[i],
-                            prime_nums[j],
-                            prime_nums[k],
-                            prime_nums[m],
-                            prime_nums[v]]
-                    prime_sets.append((set, sum(set)))
-print("End of prime set construction")
+good_quads = []
+for trip in good_trips:
+    start = trip[2]
+    for x in prime_nums:
+        if x > start:
+            if evaluate_pair([trip[0], x]) and evaluate_pair([trip[1], x]) and evaluate_pair([trip[2], x]):
+                good_quads.append([trip[0], trip[1], trip[2], x])
 
-sorted_sets = sort_list_of_tuples(prime_sets)
-for set in sorted_sets:
-    if evaluate_set(set[0]):
-        print(set)
-        break
+good_quints = []
+for quad in good_quads:
+    start = quad[3]
+    for x in prime_nums:
+        if x > start:
+            if evaluate_pair([quad[0], x]) and evaluate_pair([quad[1], x]) and evaluate_pair([quad[2], x]) and evaluate_pair([quad[3], x]):
+                good_quints.append([quad[0], quad[1], quad[2], quad[3], x])
+
+print(good_quints)
+print(sum(good_quints))
 
 print("...End")
