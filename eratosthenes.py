0b3764de528700c3be4808163977c51190b96062
Samuel Waugh <samjmwaugh@gmail.com>
2023-05-11 18:52:57 +0100

    fix erato

--- eratosthenes.py
+++ eratosthenes.py
@@ -1,5 +1,7 @@
 ### The Seive of Eratosthenes
 
+### Efficient Prime list generator
+
 from math import isqrt
 
 def primes_less_than(n):
@@ -8,16 +10,15 @@ def primes_less_than(n):
     is_prime = [True] * n
     is_prime[0] = False
     is_prime[1] = False
-
-
     for i in range(2, isqrt(n)+1):
         if is_prime[i]:
             for x in range(i*i, n, i):
                 is_prime[x] = False
+    return is_prime
 
-    return [i for i in range(n) if is_prime[i]]
-
-primes = primes_less_than(100000000)
+# True/False primes under 100,000
+prime_bools = primes_less_than(100000)
+prime_nums = [i for i in range(len(prime_bools)) if prime_bools[i]]
 
 print("Done")
 
