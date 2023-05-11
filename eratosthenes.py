de9675aaad34505d437a2f6dd7ca342bf945582c
Samuel Waugh <samjmwaugh@gmail.com>
2023-05-11 16:10:57 +0100

    Eratothenes seive

---
+++ eratosthenes.py
@@ -0,0 +1,24 @@
+### The Seive of Eratosthenes
+
+from math import isqrt
+
+def primes_less_than(n):
+    if n <= 2:
+        return []
+    is_prime = [True] * n
+    is_prime[0] = False
+    is_prime[1] = False
+
+
+    for i in range(2, isqrt(n)+1):
+        if is_prime[i]:
+            for x in range(i*i, n, i):
+                is_prime[x] = False
+
+    return [i for i in range(n) if is_prime[i]]
+
+primes = primes_less_than(100000000)
+
+print("Done")
+
+
