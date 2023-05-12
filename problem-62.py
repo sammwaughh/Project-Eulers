9725d3aa0409334a2a764b7f087219aaa0f3330a
Samuel Waugh <samjmwaugh@gmail.com>
2023-05-12 21:12:55 +0100

    Efficient is_anagram(a, b)

--- problem-62.py
+++ problem-62.py
@@ -2,6 +2,22 @@
 
 print("Running...")
 
+def is_anagram(a, b):
+    count_a = {}
+    count_b = {}
+    for c in a:
+        if c in count_a.keys():
+            count_a[c] += 1
+        else:
+            count_a[c] = 1
+    for c in b:
+        if c in count_b.keys():
+            count_b[c] += 1
+        else:
+            count_b[c] = 1
+    return count_a == count_b
+
+
 
 
 print("...End")
