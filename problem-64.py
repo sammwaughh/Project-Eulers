647aab8ee0abf8f971ce3a98248e521ecb1c25af
Samuel Waugh <samjmwaugh@gmail.com>
2024-02-10 20:47:29 +0000

    started fractional way

--- problem-64.py
+++ problem-64.py
@@ -1,15 +1,54 @@
 # Problem 64
 from math import sqrt, floor
 
-def isOddPeriod(n):
+"""def period(n):
+    root = sqrt(n)
+    floor_root = floor(sqrt(n))
+    if floor_root == root:
+        return [False, False]
+    n = 1 / (root - floor_root)
+    period_arr = [int(n)]
+    i = 0
+    while i < 10:
+        if round(n-floor_root, 4) == round(root, 4):
+            break
+        n = 1 / (n - int(n))
+        period_arr.append(int(n))
+        i += 1
+    return period_arr"""
+
+def rationaliseReciprocal(numerator, surd, integer):
+    denominator = surd - integer**2
+    numerator = (surd, integer*-1)
+    return numerator, denominator
+
+print(rationaliseReciprocal(23, -4))
+
+def getIntegerPart(numerator, denominator):
+    integerPart = int((sqrt(numerator[0]) + numerator[1])/denominator)
+    return integerPart
+
+def subtractInteger(fraction, integerPart):
     pass
+
+def period(n):
+    # Example n = 23
+    root = sqrt(n) # root = 4.795
+    adder = floor(root) # adder = 4
+    pass
+
+
+# for i in range(14):
+#     print(i, period(i))
 
 def countOddPeriods(n):
     count = 0
-    for i in range(1, n+1):
-        if floor(sqrt(i)) != sqrt(i):
-            if isOddPeriod(i):
-                count += 1
+    for i in range(2, n+1):
+        p = period(i)
+        l = len(p)
+        if l % 2 != 0:
+            count += 1
+        print(i, p, l)
     return count
 
-
+# print(countOddPeriods(100))
