49d7d36de93cad3a0f6dc797f319a9296c24c647
Samuel Waugh <samjmwaugh@gmail.com>
2024-02-22 20:19:56 +0000

    Fresh start

--- problem-72.py
+++ problem-72.py
@@ -1,19 +1,7 @@
 # Problem 72
 
 import time
-from copy import copy
-
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-def coPrimeLessThan(n):
-    count = 0
-    for i in range(1, n):
-        if gcd(i, n) == 1:
-            count += 1
-    return count
+start_time = time.time()
 
 def generatePrimeUpTo(n):
     seive = [None, None, ]
@@ -35,26 +23,14 @@ def generatePrimeUpTo(n):
             primes.append(ele)
     return primes
 
-
-def findPrimeFactors(n):
-    primes = []
-    pass
+primeList = generatePrimeUpTo(1000000)
 
 
-def newCoPrimeLessThan(n):
-    # prime-factors = findPrimeFactors(n)
-    pass
 
 
-def numberOfFractions(d):
-    total = 0
-    for i in range(1, d+1):
-        total += coPrimeLessThan(i)
-    return total
 
 
-start_time = time.time()
-print(generatePrimeSeive(1000000))
+
 # print(numberOfFractions(20000))
 end_time = time.time()
 execution_time = end_time - start_time
